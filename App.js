b64111e9b3b07112ba5d408f0c5334de6551a592
Chathuranga97lj <chathuit97@gmail.com>
2022-04-22 00:18:24 +0530

    refacturing and add anothoer services

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View } from 'react-native';
-import axios from 'axios';
-
-const getPopularMovies = async () => { 
-  const resp = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=17713b08f0f0a87d14e6f8836fc0798b");
-  return resp.data.results;
-};
+import { getPopularMovies } from './services/services';
 
 const App = () => {
 
---
+++ services/services.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = 'api_key=17713b08f0f0a87d14e6f8836fc0798b';
+
+// Get popular Movies
+export const getPopularMovies = async () => { 
+    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+    return resp.data.results;
+};
+
+// Get upcomming Movies
+export const getUpcomingMovies = async () => { 
+    const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+    return resp.data.results;
+};
+
+// Get popular TV
+export const getPopularTv = async () => { 
+    const resp = await axios.get(`${apiUrl}/tv/upcoming?${apiKey}`);
+    return resp.data.results;
+};
