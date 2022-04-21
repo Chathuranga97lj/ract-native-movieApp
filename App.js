040be2f8d71112565537e9ecc10442657907f685
Chathuranga97lj <chathuit97@gmail.com>
2022-04-21 23:45:19 +0530

    display movie data

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
 
 const getPopularMovies = async () => { 
   const resp = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=17713b08f0f0a87d14e6f8836fc0798b");
-  console.log(JSON.stringify(resp.data.results[0], null, 2))
-}
+  return resp.data.results;
+};
 
 const App = () => {
-  getPopularMovies();
+
+  const [movie, setMovie] = useState('');
+
+  getPopularMovies().then(movies => {
+    setMovie(movies[0]);
+  });
+
+
   return (
     <View
       style={{
@@ -16,7 +23,9 @@ const App = () => {
         justifyContent: "center",
         alignItems: "center"
       }}>
-      <Text>Hello, world!</Text>
+      <Text>Movie Name: {movie.original_title}</Text>
+      <Text>Movie Langulage: {movie.original_language}</Text>
+      <Text>Release Date: {movie.release_date}</Text>
     </View>
   )
 }
