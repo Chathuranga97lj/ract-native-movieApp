677e08c17c227fad1d4b4a6cb7e0cc563685f302
Chathuranga97lj <chathuit97@gmail.com>
2022-04-21 23:28:29 +0530

    add movies data services

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () => { 
+  const resp = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=17713b08f0f0a87d14e6f8836fc0798b");
+  console.log(JSON.stringify(resp.data.results[0], null, 2))
+}
 
 const App = () => {
+  getPopularMovies();
   return (
     <View
       style={{
