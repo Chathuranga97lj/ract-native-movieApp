35f3ce632412e2f01f4beed82a9da2a3d9ef1c5e
Chathuranga97lj <chathuit97@gmail.com>
2022-04-25 09:36:32 +0530

    add loading spinner

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet, Dimensions, ScrollView} from 'react-native';
+import {Text, View, StyleSheet, Dimensions, ScrollView, ActivityIndicator} from 'react-native';
 import {getUpcomingMovies, getPopularMovies, getPopularTv, getFamilyMovies} from '../services/services';
 import {SliderBox} from 'react-native-image-slider-box';
 import List from '../components/List'; // pure componnet call without brackets
@@ -12,6 +12,7 @@ const Home = () => {
   const [popularTv, setPopularTv] = useState();
   const [familyMovies, setFamiliyMovies] = useState();
   const [error, setError] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   const getData = () => {
     return Promise.all([
@@ -37,51 +38,55 @@ const Home = () => {
           setPopularMovies(popularMoviesData);
           setPopularTv(popularTvData);
           setFamiliyMovies(familyMoviesData);
-
         },
       )
       .catch(err => {
         setError(err);
+      })
+      .finally(() => {
+        setLoaded(true);
       });
   }, []);
 
   return (
     <React.Fragment>
-      <ScrollView>
-        {/* upcoming movie image moving slider */}
-        {moviesImages && (
-          <View style={styles.stliderContainer}>
-            <SliderBox
-              images={moviesImages}
-              dotStyle={styles.sliderStyle}
-              sliderBoxHeight={dimensions.height / 1.5}
-              autoplay={true}
-              circleLoop={true}
-            />
-          </View>)}
+      {loaded && (
+        <ScrollView>
+          {/* upcoming movie image moving slider */}
+          {moviesImages && (
+            <View style={styles.stliderContainer}>
+              <SliderBox
+                images={moviesImages}
+                dotStyle={styles.sliderStyle}
+                sliderBoxHeight={dimensions.height / 1.5}
+                autoplay={true}
+                circleLoop={true}
+              />
+            </View>)}
 
-        {/* popular moveies   */}
-        {popularMovies && (
-          <View style={styles.carousel}>
-            <List title="Popular Movies" content={popularMovies} />
-          </View>
-        )}
+          {/* popular moveies   */}
+          {popularMovies && (
+            <View style={styles.carousel}>
+              <List title="Popular Movies" content={popularMovies} />
+            </View>
+          )}
 
-        {/* popular tv shows   */}
-        {popularTv && (
-          <View style={styles.carousel}>
-            <List title="Popular TV Shows" content={popularTv} />
-          </View>
-        )}
+          {/* popular tv shows   */}
+          {popularTv && (
+            <View style={styles.carousel}>
+              <List title="Popular TV Shows" content={popularTv} />
+            </View>
+          )}
      
-        {/* family movies   */}
-        {familyMovies && (
-          <View style={styles.carousel}>
-            <List title="Family Movies" content={familyMovies} />
-          </View>
-        )}    
-      
-      </ScrollView>  
+          {/* family movies   */}
+          {familyMovies && (
+            <View style={styles.carousel}>
+              <List title="Family Movies" content={familyMovies} />
+            </View>
+          )}
+        </ScrollView>
+    )}
+      {!loaded && <ActivityIndicator size={'large'} />}  
     </React.Fragment>
   );
 };
